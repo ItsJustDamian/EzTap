325338314b611b38e1f96f21013958df56bf8a2a
quin <info@quintendct.nl>
2021-02-09 10:00:40 +0100

    break more shit

--- EzTap/features/Misc.cpp
+++ EzTap/features/Misc.cpp
@@ -6,7 +6,7 @@ void Misc::Bunnyhop(CUserCmd* pCmd)
 		return;
 
 	static auto wasLastTimeOnGround{ LocalPlayer->flags() & 1 };
-
+	
 	if (!(LocalPlayer->flags() & 1) && LocalPlayer->moveType() != MoveType::LADDER && !wasLastTimeOnGround)
 		pCmd->buttons &= ~IN_JUMP;
 
@@ -42,9 +42,15 @@ void Misc::NoFlash(bool enabled)
 	LocalPlayer->SetflashAlpha((enabled ? 0.f : 255.f));
 }
 
-static int delay = 0;
-
 void Misc::ClanTag()
 {
-	Utils::SetClantag("EzTap.gg");
+	float time = interfaces.GlobalVars->realtime;
+	std::string clantag = "PETERHOOK ";
+	std::string curClantag;
+	for (int i = 0; i < clantag.length(); i++) {
+		int curLetter = i + (int)(time * 2); // Rate is 2 characters a second
+		curClantag += clantag[curLetter % clantag.length()]; // Current char is wrapped to clantag length so it loops
+	}
+
+	Utils::SetClantag(curClantag.c_str());
 }
